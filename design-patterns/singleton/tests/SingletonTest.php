1cdbc3ddcbe952fcf23d58a3d820415e25cb223b
Gideon <git@mortolian.com>
2023-06-25 19:56:09 +0200

    ADD: Adds the singleton test file.

---
+++ design-patterns/singleton/tests/SingletonTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SingletonTest extends TestCase
+{
+    public function testSingleton(): void
+    {
+        $this->assertTrue(true);
+    }
+}
