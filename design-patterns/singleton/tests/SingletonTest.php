ba56db5937172f81337c8c5e60b97ced01c9fe34
Gideon <git@mortolian.com>
2023-06-26 00:02:01 +0200

    ADD: Tests for singleton.

--- design-patterns/singleton/tests/SingletonTest.php
+++ design-patterns/singleton/tests/SingletonTest.php
@@ -2,12 +2,52 @@
 
 namespace App\Tests;
 
+use App\Config;
+use App\Logger;
+use Error;
+use Exception;
 use PHPUnit\Framework\TestCase;
 
 class SingletonTest extends TestCase
 {
-    public function testSingleton(): void
+    public function testLoggerInstance(): void
     {
-        $this->assertTrue(true);
+        Logger::log("Testing");
+
+        $l1 = Logger::getInstance();
+        $l2 = Logger::getInstance();
+
+        $this->assertTrue($l1 === $l2);
+    }
+
+    public function testConfigSetup(): void
+    {
+        $config1 = Config::getInstance();
+
+        $login = "test_login";
+        $password = "test_password";
+
+        $config1->setValue("login", $login);
+        $config1->setValue("password", $password);
+
+        $config2 = Config::getInstance();
+
+        $this->assertEquals($config2->getValue("login"), $login);
+        $this->assertEquals($config2->getValue("password"), $password);
+    }
+
+    public function testUnserialisationShouldFail(): void
+    {
+        $this->expectException(Exception::class);
+        $logger_serialized = serialize(Logger::getInstance());
+        unserialize($logger_serialized);
+    }
+
+    public function testCloneShouldFail(): void
+    {
+        $this->expectException(Error::class);
+
+        $logger = Logger::getInstance();
+        $logger2 = clone($logger);
     }
 }
